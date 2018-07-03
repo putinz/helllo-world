78312d7183f3acc28202c5124f9e1737da92c468
Putinz <ivan@putincoin.org>
2018-07-03 16:28:52 +0200

    2. List Candidates

--- election/contracts/Election.sol
+++ election/contracts/Election.sol
@@ -4,11 +4,28 @@ pragma solidity ^0.4.24;
  * The Election contract is a test for creating a SMC
  */
 contract Election {
-	// Store Candidate
-	// Read Candidate
-	string public candidate;
-	// Constructor
+	// Model a Candidate
+	struct Candidate {
+		uint id;
+		string name;
+		uint voteCount;
+	}
+	
+	// Store Candidates
+
+	// Fetch Candidate
+	mapping (uint => Candidate) public candidates;
+	
+	// Store Candidates Count
+	uint public candidatesCount;
+
 	function Election () public {
-		candidate = "EOS";
-	}	
+		addCandidate("EOS");
+		addCandidate("ETH");
+	}
+
+	function addCandidate (string _name) private {
+		candidatesCount ++;
+		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);	
+	}
 }
